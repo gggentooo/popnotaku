243e906e6f1e25c5b333822ac2230a07edff6445
gggentooo <gggentooo@gmail.com>
2025-06-11 15:06:35 +0900

    Uploaded main files

--- src/js/common.js
+++ src/js/common.js
@@ -17,12 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementsByTagName('header')[0].innerHTML = `
         <span class="pagetitle" onclick="location.href='https://popnotaku.lnaba.reisen/'">popnotaku</span>
-        <button class="darkmode" onclick="toggleDark()">Toggle Dark Mode</button>
+        <button class="darkmode" onclick="toggleDark()">다크 모드 켜기/끄기</button>
         <nav>
-            <a href="https://popnotaku.lnaba.reisen/">Menu</a>
-        </nav>`;
-        
-    document.getElementsByTagName('footer')[0].innerHTML = `<a href="mailto:d@lnaba.reisen">Made and Written by D</a>`;
+            <a href="https://popnotaku.lnaba.reisen/">홈으로</a>
+        </nav>
+        `;
+
+    document.getElementsByTagName('footer')[0].innerHTML = `
+        <a href="mailto:d@lnaba.reisen">사이트 운영: D</a>
+        `;
 });
 
 function langOpenClose(target_lang) {
