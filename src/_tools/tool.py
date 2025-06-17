a369e29834786f910d5c304ef86cbcd95967087a
gggentooo <gggentooo@gmail.com>
2025-06-17 10:47:13 +0900

    added song list

--- src/_tools/tool.py
+++ src/_tools/tool.py
@@ -81,13 +81,22 @@ def generate_lounge_json():
     json.dump(out, outfile, ensure_ascii=False)
 
 def generate_history_individual_json():
-    with open('../data/popnhistory.json', 'r') as source:
+    with open('../data/title/arcade_titles.json', 'r') as source:
         raw = json.load(source)
     
     for title in raw:
         num = title["number"]
+        obj = {
+            "number": title["number"],
+            "title": title["title"],
+            "title-ko": title["title-ko"],
+            "release": title["release"],
+            "sections": [
+                
+            ]
+        }
         with open(f'../data/title/{num}.json', 'w') as file:
-            json.dump(title, file, ensure_ascii=False)
+            json.dump(obj, file, ensure_ascii=False)
             
 def generate_genre_strings_from_html():
     out = []
@@ -141,8 +150,100 @@ def generate_song_jsonframes(title):
             with open(f'../data/song/{title}/{s}.json', 'w', encoding='utf8') as out:
                 json.dump(frame, out, ensure_ascii=False)
 
+def generate_songlist_0617():
+    out = []
+    
+    songnum = 0
+    idx = 0
+    idnum = -1
+    
+    idstrings = []
+    
+    genrestr = ""
+    titlestr = ""
+    artiststr = ""
+    
+    with open('./tempanother.txt', 'r') as idlist:
+        for line in idlist:
+            idstrings.append(line.strip('\n'))
+    
+    with open('./temp.txt', 'r') as source:
+        for line in source:
+            idx += 1
+            if "<tr>" in line:
+                songnum += 1
+                idx = 0
+            if idx == 1:
+                genrestr = line.split(">")[1].split("<")[0]
+            if idx == 2:
+                titlestr = line.split(">")[1].split("<")[0]
+            if idx == 3:
+                artiststr = line.split(">")[1].split("<")[0]
+            if "</tr>" in line:
+                idnum += 1
+                if idnum >= len(idstrings):
+                    break
+                urlstr = idstrings[idnum]
+                songobj = {
+                    "id": "",
+                    "genre": genrestr,
+                    "title": titlestr,
+                    "artist": artiststr
+                }
+                out.append(songobj)
+    
+    outfile = open('./temp2.json', 'w', encoding='utf8')
+    json.dump(out, outfile, ensure_ascii=False)
+
+def convert_18_songlist():
+    obj_18 = {}
+    frames = []
+    
+    with open('../data/title/18.json', 'r') as json_18:
+        obj_18 = json.load(json_18)
+    with open('./temp2.json', 'r') as fr:
+        frames = json.load(fr)
+    
+    for cat in obj_18["songs"]:
+        ls = cat["list"]
+        for idx, entry in enumerate(ls):
+            for f in frames:
+                if f["id"] == entry:
+                    entry = f
+            ls[idx] = entry
+
+    outfile = open('./temp.json', 'w', encoding='utf8')
+    json.dump(obj_18, outfile, ensure_ascii=False)
+    
+def sift_rawdata():
+    out = []
+    
+    with open('./data.json', 'r') as raw:
+        dat = json.load(raw)
+        for entry in dat:
+            if entry["hardest"]:
+                obj = {
+                    "id": entry["sid"],
+                    "title": entry["title"],
+                    "fw-title": entry["fwTitle"],
+                    "r-title": entry["rTitle"],
+                    "genre": entry["genre"],
+                    "fw-genre": entry["fwGenre"],
+                    "r-genre": entry["rGenre"],
+                    "artist": entry["artist"],
+                    "debut": entry["debut"],
+                    "slug": entry["slug"],
+                    "remy": entry["remyPath"],
+                    "chara": entry["rChara"]
+                }
+                out.append(obj)
+    
+    outfile = open('./data_sifted.json', 'w', encoding='utf8')
+    json.dump(out, outfile, ensure_ascii=False)
+    
+
 def main():
-    generate_song_jsonframes(18)
+    generate_history_individual_json()
 
 if __name__ == "__main__":
     main()
